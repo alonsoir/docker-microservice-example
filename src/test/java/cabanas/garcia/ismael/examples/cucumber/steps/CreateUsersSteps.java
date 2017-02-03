4e214d47280282f31cc4c89854e640bd74598844
XI317311 <ismael.cabanas@extern.isban.com>
2017-02-03 13:28:04 +0100

    Añadidas capacidades Spring Boot a tests
    En este commit he añadido la capacidades Spring Boot a los tests de
    Cucumber.

--- src/test/java/cabanas/garcia/ismael/examples/cucumber/steps/CreateUsersSteps.java
+++ src/test/java/cabanas/garcia/ismael/examples/cucumber/steps/CreateUsersSteps.java
@@ -1,17 +1,21 @@
 package cabanas.garcia.ismael.examples.cucumber.steps;
 
+import cabanas.garcia.ismael.examples.UserManagementApplication;
 import cabanas.garcia.ismael.examples.cucumber.common.Attribute;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
-import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 import java.util.List;
 
 /**
  * Created by XI317311 on 03/02/2017.
  */
+@SpringBootTest(classes = UserManagementApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration
 public class CreateUsersSteps {
     @When("^the client requests for creating (.*) user$")
     public void theClientRequestsForCreatingUser(String userName) throws Throwable {
